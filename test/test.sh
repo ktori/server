20223c68a41671912c951e6b1b68f43fbd047e0f
Victoria Kabaeva <tori@ktori.me>
2020-02-16 20:23:27 +0300

    ci: fix make

--- test/test.sh
+++ test/test.sh
@@ -2,9 +2,9 @@
 
 set -e
 
+trap "kill %1" EXIT
+
 build/server &
 
 [[ "200" = $(curl -sL -w "%{http_code}\\n" "http://127.0.0.1:8080" -o /dev/null) ]]
 [[ "200" = $(curl --insecure -sL -w "%{http_code}\\n" "https://127.0.0.1:8081" -o /dev/null) ]]
-
-kill %1
